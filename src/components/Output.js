b34b3c1a3e911b3c87a6ab099e591eca53dae897
benjaminboruff <ben@boruff.me>
2017-01-16 23:08:32 -0500

    formatted seconds to two digits

--- src/components/Output.js
+++ src/components/Output.js
@@ -8,7 +8,7 @@ function Output(props) {
     return (
       <div>
         <h2> Output: </h2>
-        <h3> Break { props.breakMin } : {props.breakSec} </h3>
+        <h3> Break { props.breakMin }:{ props.breakSec < 10 ? "0" + props.breakSec : props.breakSec } </h3>
         <h1>Take a break!</h1>
         <Button raised colored onClick={props.handleStartStop}>Start/Stop</Button>
       </div>
@@ -17,8 +17,8 @@ function Output(props) {
     return (
       <div >
         <h2> Output: </h2>
-        <h3> Break time  { props.breakMin }:{props.breakSec} </h3>
-        <h3> Session time { props.sessionMin }:{ props.sessionSec } </h3>
+        <h3> Break time  { props.breakMin }:{ props.breakSec < 10 ? "0" + props.breakSec : props.breakSec } </h3>
+        <h3> Session time { props.sessionMin }:{ props.sessionSec < 10 ? "0" + props.sessionSec : props.sessionSec } </h3>
         <Button raised colored onClick={props.handleStartStop}>Start/Stop</Button>
       </div>
     );
