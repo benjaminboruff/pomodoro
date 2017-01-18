d8ba5630b68dd5b716cd1328c91fc0d8b95ea48a
benjaminboruff <ben@boruff.me>
2017-01-18 13:07:51 -0500

    design part4

--- src/components/SetBreak.js
+++ src/components/SetBreak.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import 'react-mdl/extra/material.css';
 import 'react-mdl/extra/material.js';
-import { Button } from 'react-mdl';
+import { FABButton } from 'react-mdl';
 import "./SetBreak.css";
 
 function SetBreak(props) {
   return (
     <div className="setbreak">
-      <Button className="break-button" value="dec-break" raised onClick={props.handlePeriodAdjust}>{'\uFF0D'}</Button>
+      <FABButton className="break-button" value="dec-break" raised onClick={props.handlePeriodAdjust}>{'\uFF0D'}</FABButton>
         <span>Break: { props.breakTime } min</span>
-      <Button className="break-button" value="inc-break" raised onClick={props.handlePeriodAdjust}>{'\uFF0B'}</Button>
+      <FABButton className="break-button" value="inc-break" raised onClick={props.handlePeriodAdjust}>{'\uFF0B'}</FABButton>
     </div>
   );
 }
--- src/components/SetSession.js
+++ src/components/SetSession.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import 'react-mdl/extra/material.css';
 import 'react-mdl/extra/material.js';
-import { Button } from 'react-mdl';
+import { FABButton } from 'react-mdl';
 import "./SetSession.css";
 
 function SetSession(props) {
   return (
     <div className="setsession">
-      <Button className="session-button" value="dec-session" raised onClick={props.handlePeriodAdjust}>{'\uFF0D'}</Button>
+      <FABButton className="session-button" value="dec-session" raised onClick={props.handlePeriodAdjust}>{'\uFF0D'}</FABButton>
         <span>Session: { props.sessionTime } min</span>
-      <Button className="session-button" value="inc-session" raised onClick={props.handlePeriodAdjust}>{'\uFF0B'}</Button>
+      <FABButton className="session-button" value="inc-session" raised onClick={props.handlePeriodAdjust}>{'\uFF0B'}</FABButton>
     </div>
   );
 }
